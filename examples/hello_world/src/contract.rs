95797fd403d46b8c71a13db233c6ebc6663c04ba
Mostafa <mostafa.sedaghat@gmail.com>
2022-07-24 18:06:02 +0800

    Updating hello_world example

--- examples/hello_world/src/contract.rs
+++ examples/hello_world/src/contract.rs
@@ -2,20 +2,33 @@ use crate::error::Error;
 use kelk::alloc::string::String;
 use kelk::context::Context;
 use kelk::kelk_derive;
+use kelk::storage::str::StorageString;
 
 #[kelk_derive(instantiate)]
 pub fn instantiate(ctx: Context, _: ()) -> Result<(), Error> {
-    Ok(ctx.storage.write_string(0, "hello world!", 16)?)
+    let mut storage_string = StorageString::create(ctx.storage, 64)?;
+    ctx.storage.fill_stack_at(1, storage_string.offset())?;
+    storage_string.set_string("hello world!")?;
+
+    Ok(())
 }
 
 #[kelk_derive(process)]
 pub fn process(ctx: Context, msg: String) -> Result<(), Error> {
-    Ok(ctx.storage.write_string(0, &msg, 16)?)
+    let storage_string_offset = ctx.storage.read_stack_at(1)?;
+    let mut storage_string = StorageString::load(ctx.storage, storage_string_offset)?;
+    storage_string.set_string(&msg)?;
+
+    Ok(())
 }
 
 #[kelk_derive(query)]
 pub fn query(ctx: Context, _: ()) -> Result<String, Error> {
-    Ok(ctx.storage.read_string(0, 16)?)
+    let storage_string_offset = ctx.storage.read_stack_at(1)?;
+    let storage_string = StorageString::load(ctx.storage, storage_string_offset)?;
+    let msg = storage_string.get_string()?;
+
+    Ok(msg)
 }
 
 #[cfg(test)]
