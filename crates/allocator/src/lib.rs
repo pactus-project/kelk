3e3f2dd0105e16190909a7fee698536158237f35
Mostafa <mostafa.sedaghat@gmail.com>
2025-06-08 19:46:34 +0800

    chore: fix clippy issues

--- crates/allocator/src/lib.rs
+++ crates/allocator/src/lib.rs
@@ -1,16 +1,15 @@
 //! Kelk-allocator providing memory allocator support for smart contracts in [Pactus](https://pactus.org/) blockchain.
-#![cfg_attr(no_std, feature(core_intrinsics, lang_items, alloc_error_handler))]
-// #![cfg_attr(no_std, feature(core_intrinsics, lang_items, alloc_error_handler))]
+#![cfg_attr(feature = "no-std", feature(core_intrinsics, lang_items, alloc_error_handler))]
 
 // Use `wee_alloc` as the global allocator.
-#[cfg(no_std)]
+#[cfg(feature = "no-std")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 /// Need to provide a tiny `panic` implementation for `#![no_std]`.
 /// This translates into an `unreachable` instruction that will
 /// raise a `trap` the WebAssembly execution if we panic at runtime.
-#[cfg(no_std)]
+#[cfg(feature = "no-std")]
 #[panic_handler]
 #[no_mangle]
 pub fn panic(_info: &::core::panic::PanicInfo) -> ! {
@@ -19,7 +18,7 @@ pub fn panic(_info: &::core::panic::PanicInfo) -> ! {
 
 /// Need to provide an allocation error handler which just aborts
 /// the execution with trap.
-#[cfg(no_std)]
+#[cfg(feature = "no-std")]
 #[alloc_error_handler]
 #[no_mangle]
 fn oom(_: core::alloc::Layout) -> ! {
@@ -27,7 +26,7 @@ fn oom(_: core::alloc::Layout) -> ! {
 }
 
 /// Needed for non-wasm targets.
-#[cfg(no_std)]
+#[cfg(feature = "no-std")]
 #[lang = "eh_personality"]
 #[no_mangle]
 pub extern "C" fn eh_personality() {}
